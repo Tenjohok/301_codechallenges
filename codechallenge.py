96f69d2fa27d363fc75c35a2794091cdc90e85e1
Tenjohok <akahtenjohokwen@gmail.com>
2023-09-08 22:02:30 +0000

    ops challenge 09

---
+++ codechallenge.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+# Script Name:                 Python scrip
+# Author:                       Akah 
+# Date of latest revision:      09/07/2023
+# Purpose:                    Create an if statement using logical conditions  and include elif
+ #                             Create an if statement that includes both elif and else to execute when both if and elif are not met.   
+ 
+# reference                               chatgpt 
+# # This code checks if two numbers are equal.
+a = 20
+b = 20
+if a == b:
+  print("The numbers are equal.")
+# This code checks if two numbers are not equal.
+a = 10
+b = 20
+if a != b:
+  print("The numbers are not equal.")
+# This code checks if a number is less than another number.
+a = 15
+b = 30
+if a < b:
+  print("a is less than b.")
+# This code checks if a number is less than or equal to another number.
+a = 10
+b = 10
+if a <= b:
+  print("a is less than or equal to b.")
+
+# This code checks if a number is greater than another number.
+a = 10
+b = 20
+
+if a > b:
+  print("a is greater than b.")
+
+# This code checks if a number is greater than or equal to another number.
+a = 10
+b = 10
+
+if a >= b:
+  print("a is greater than or equal to b.")
+
